a7b50eb0ae9de71dc9d51d346367b42bb3ce3d29
Denis Shalagin <dshalagin@weezlabs.com>
2015-05-29 13:25:14 +0300

    initial commit

---
+++ test_ci/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_ci
+//
+//  Created by Denis Shalagin on 5/29/15.
+//  Copyright (c) 2015 Denis Shalagin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_ci/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_ci
+//
+//  Created by Denis Shalagin on 5/29/15.
+//  Copyright (c) 2015 Denis Shalagin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
